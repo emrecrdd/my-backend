b48a87da75f446c11e348cc1a4b513c9e4066b11
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-04-11 02:46:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 // HTTP server'ı başlatıyoruz
