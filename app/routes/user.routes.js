596fbb9c4ff0c6f9745e211a0d4f7886486e7dda
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-04-14 02:18:23 +0300

    Update user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -8,11 +8,7 @@ module.exports = app => {
    
     // Kullanıcı girişi (Login)
     router.post("/login", users.login); 
-    router.post("/forgot-password", users.forgotPassword);
-
-// Yeni şifre kaydetme (şifre sıfırlama linkinden sonra)
-// Yeni şifre kaydetme (şifre sıfırlama linkinden sonra)
-router.post("/reset-password/:token", users.resetPassword);
+  
    // Kullanıcı Profili Alma (GET /api/users/:id)
     router.get("/:id", users.getUserProfile);
      // Kullanıcı Bilgilerini Güncelleme (PUT /api/users/:id)
