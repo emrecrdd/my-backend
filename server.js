15432e3e213ae939a0c8e10bf6f581fa70323e4e
Emre <121616048+emrecrdd@users.noreply.github.com>
2026-01-03 05:08:45 +0300

    Update CORS options to use HTTPS for Netlify

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 
 var corsOptions = {
-  origin: ["http://localhost:3000", "ilmoradesign.netlify.app"], // Netlify frontend domain'i
+  origin: ["http://localhost:3000", "https://ilmoradesign.netlify.app"], // Netlify frontend domain'i
   credentials: true
 };
 
